390227bfb2b3578a8b5e76641c2d49b2ce6926ef
Cam Nguyen <cnguyen1187@gmail.com>
2018-01-05 04:15:02 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,14 @@ end
 def sort_array_char_count(array)
   array.sort {|a,b| a.length <=> b.length}
 end
+
+def swap_elements(array)
+  array.insert(1, array[2]).pop
+  array
+end
+
+def reverse_array(array)
+  new_arr = []
+  array.reverse_each{|i| new_arr << i}
+  new_arr
+end
